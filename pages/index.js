952f1a2f89116e6348c1ef8d0cafbb4c0d420d47
Wescley Garrett <145073395+c1ph3r-z3r0@users.noreply.github.com>
2025-01-23 21:04:10 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>Home Page Tabnews em Construção</h1>
+function Home() {
+  return <h1>Home Page Tabnews em Construção</h1>;
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
